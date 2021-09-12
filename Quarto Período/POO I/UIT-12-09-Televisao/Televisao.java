5b2f2e6b5a9647c2414a8f7fab4d3f8a62e8b9b3
Eric Henrique de Castro Chaves <eric-henrique44@hotmail.com>
2021-09-12 18:28:48 -0300

    Atividade Televisão

---
+++ Quarto Período/POO I/UIT-12-09-Televisao/Televisao.java
@@ -0,0 +1,130 @@
+/* Crie uma classe Televisao com métodos que podem controlar o volume e trocar os canais da televisão. A TV só funciona ligada e deve ter limites de volume e canais. 
+Os canais ao chegar no último volta para o primeiro. Crie os seguintes controles: aumentar ou diminuir a potência do volume de som em uma unidade de cada vez; 
+aumentar e diminuir o número do canal em uma unidade trocar para um canal indicado ou subir um canal caso não seja informado o canal; consultar o valor do volume de som e o 
+canal selecionado. Crie dois construtores para a TV, um que recebe apenas a marcad da TV e outro que recebe a marca, quantidade de canais e volume máximo. 
+Crie o método toString que mostrará a marca da TV, o volume e o canal atual. Crie menu para interagir com a TV. Utilize métodos Get e Set. */ 
+
+public class Televisao {
+    private int volumeMaximo;
+    private int quantidadeCanais;
+    private String marca;
+    private boolean ligada;
+    private int volume;
+    private int canal;
+    
+    
+    
+    public Televisao(String marca) {
+        this.marca = marca;
+    }
+
+    
+
+    public Televisao(String marca, int quantidadeCanais, int volumeMaximo) {
+        this.volumeMaximo = volumeMaximo;
+        this.quantidadeCanais = quantidadeCanais;
+        this.marca = marca;
+    }
+
+
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public boolean isLigada() {
+        return ligada;
+    }
+
+    public void ligar(){
+        ligada=true;
+    }
+
+    public void desligar(){
+        ligada=false;
+    }
+    
+    public int getVolume() {
+        return volume;
+    }
+
+    public void aumentarVolume(){
+        if(ligada==true && volume>=0 && volume<volumeMaximo){
+            volume++;
+        }else{
+            volume=volumeMaximo;
+        }
+    }
+
+    public void abaixarVolume(){
+        if(ligada==true && volume>0 && volume<=volumeMaximo){
+            volume--;
+        }
+    }
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        if(ligada==true && canal>0 && canal<=quantidadeCanais){
+        this.canal = canal;
+        }
+    }
+
+    public void aumentarCanal(){
+        if(canal>=0 && canal<quantidadeCanais){
+            canal++;
+        }else if(canal>=quantidadeCanais){
+            canal=0;
+        }
+    }
+
+    public void abaixarCanal(){
+        if(canal>0 && canal<quantidadeCanais){
+            canal--;
+        }else if(canal==0){
+            canal=quantidadeCanais;
+        }
+    }
+
+    
+
+    public int getVolumeMaximo() {
+        return volumeMaximo;
+    }
+
+
+
+    public void setVolumeMaximo(int volumeMaximo) {
+        this.volumeMaximo = volumeMaximo;
+    }
+
+
+
+    public int getQuantidadeCanais() {
+        return quantidadeCanais;
+    }
+
+
+
+    public void setQuantidadeCanais(int quantidadeCanais) {
+        this.quantidadeCanais = quantidadeCanais;
+    }
+
+
+
+    public String imprimeDetalhe(){
+        return  " O a teleevisão está "+ (isLigada()?"ligado":"desligado ") +
+                "\n Volume: " + getVolume() + 
+                "\n Canal: " + getCanal();
+    }
+
+
+
+    public String toString() {
+        return "Televisao [canal=" + canal + ", marca=" + marca + ", volume=" + volume + "]";
+    }
+    
+    
+
+}
---
+++ Quarto Período/POO I/UIT-12-09-Televisao/UIT1209Televisao.java
@@ -0,0 +1,75 @@
+/* Crie uma classe Televisao com métodos que podem controlar o volume e trocar os canais da televisão. A TV só funciona ligada e deve ter limites de volume e canais. 
+Os canais ao chegar no último volta para o primeiro. Crie os seguintes controles: aumentar ou diminuir a potência do volume de som em uma unidade de cada vez; 
+aumentar e diminuir o número do canal em uma unidade trocar para um canal indicado ou subir um canal caso não seja informado o canal; consultar o valor do volume de som e o 
+canal selecionado. Crie dois construtores para a TV, um que recebe apenas a marcad da TV e outro que recebe a marca, quantidade de canais e volume máximo. 
+Crie o método toString que mostrará a marca da TV, o volume e o canal atual. Crie menu para interagir com a TV. Utilize métodos Get e Set. */ 
+
+import java.util.Scanner;
+
+public class UIT1209Televisao {
+   
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Informe a Marca da Televisão:");
+        String marca = input.nextLine();
+        System.out.println("Informe a Quantidade Maxia de canais:");
+        int quantidadeCanais = input.nextInt();
+        System.out.println("Informe o Volume Maximo da Televisão:");
+        int volumeMaximo= input.nextInt();
+
+        Televisao televisao = new Televisao(marca, quantidadeCanais, volumeMaximo);
+
+        System.out.println(televisao);
+
+        int opcao=0;
+        do{
+            System.out.println("Digite o que deseja:");
+            System.out.println("0-Sair");
+            System.out.println("1-Ligar");
+            System.out.println("2-Desligar");
+            System.out.println("3-Informar canal desejado");
+            System.out.println("4-Up Mudar Canal");
+            System.out.println("5-Down Mudar Canal");
+            System.out.println("6-Aumentar Volume ");
+            System.out.println("7- Abaixar Volume");
+            System.out.println("8-Imprimir Detalhe");
+            opcao=input.nextInt();
+            switch(opcao){
+                case 0:
+                    System.out.println("Muito Obrigado ate Mais.");
+                    break;
+                case 1:
+                    televisao.ligar();
+                    break;
+                case 2:
+                    televisao.desligar();
+                    break;
+                case 3:
+                    System.out.println("Informe o canal Desejado:");
+                    televisao.setCanal(input.nextInt());
+                    break;
+                case 4:
+                    televisao.aumentarCanal();
+                    break;
+                case 5:
+                    televisao.abaixarCanal();;
+                    break;
+                case 6:
+                    televisao.aumentarVolume();
+                    break;
+                case 7:
+                    televisao.abaixarVolume();
+                    break;
+                case 8:
+                    System.out.println(televisao.imprimeDetalhe());
+                    break;
+                default:
+                    System.out.println("Opção Inválida");
+            }        
+        }while(opcao!=0);
+        
+        input.close();
+    }
+}
