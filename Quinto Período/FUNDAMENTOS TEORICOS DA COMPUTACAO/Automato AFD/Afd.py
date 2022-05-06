d6ee243b62c4f6de6f6914cb86af286de2660b4a
Eric Henrique de Castro Chaves <eric-henrique44@hotmail.com>
2022-05-06 18:14:07 -0300

    Implementando Metodo Para buscar do Arquivo

--- Quinto Período/FUNDAMENTOS TEORICOS DA COMPUTACAO/Automato AFD/Afd.py
+++ Quinto Período/FUNDAMENTOS TEORICOS DA COMPUTACAO/Automato AFD/Afd.py
@@ -1,8 +1,49 @@
-transicao = {('S0', 0) : 'S0', ('S0', 1) : 'S1', ('S1', 0) : 'S2', ('S1', 1) : 'S0', ('S2', 0) : 'S1', ('S2', 1) : 'S2'}
-alfabeto = set(['0','1'])
+import re
+transicao = {('S0', 0): 'S0', ('S0', 1): 'S1', ('S1', 0): 'S2',
+             ('S1', 1): 'S0', ('S2', 0): 'S1', ('S2', 1): 'S2'}
+alfabeto = set(['0', '1'])
 estadoInicial = 'S0'
 estadoFinal = 'S0'
 automato = '1001'
+
+
+def lerArquivo():
+    arquivo = open('entrada.txt', 'r')
+    automato = []
+    for linha in arquivo:
+        automato.append(linha.strip())
+    return automato
+
+
+def separarEstados(automato):
+    estados = re.split(" ", automato[1])  # Os estados estão na linha 1
+    return estados
+
+
+def separarAlfabeto(automato):
+    alfabeto = re.split(" ", automato[2])  # Os alfabetos estão na linha 2
+    return alfabeto
+
+
+def pegarEstadoInicial(automato):
+    estadoInicial = automato[3]  # O estado inicial esta na linha 3
+    return estadoInicial
+
+
+def pegarEstadoFinal(automato):
+    estadoFinal = automato[4]  # O estado final esta na linha 4
+    return estadoFinal
+
+
+def separarTransicoes(automato):
+    transicao = {}
+    for i in range(5, len(automato)):
+        item = automato[i].split()
+        aux = item[0]
+        aux2 = item[2]
+        transicao[aux, int(aux2)] = item[1]
+    return transicao
+
 
 class Afd:
     def __init__(self, automato, estadoInicial, alfabeto, transicao, estadoFinal):
@@ -18,13 +59,13 @@ class Afd:
         for i in self.automato:
             if i in self.alfabeto:
                 estadoAtual = self.transicao[estadoAtual, int(i)]
-                sequenciaAutomato = sequenciaAutomato + ' ' +estadoAtual
+                sequenciaAutomato = sequenciaAutomato + ' ' + estadoAtual
             else:
                 sequenciaAutomato = 'ERRO'
                 break
         return sequenciaAutomato
-        
-    def validarAfd(self, sequenciaAutomato): 
+
+    def validarAfd(self, sequenciaAutomato):
         self.sequenciaAutomato = sequenciaAutomato
         if self.sequenciaAutomato != 'ERRO':
             if self.sequenciaAutomato[-1] == self.estadoFinal[-1]:
@@ -33,9 +74,25 @@ class Afd:
                 return False
         return sequenciaAutomato
 
-            
 
-sequenciaAutomato = Afd(automato, estadoInicial, alfabeto, transicao, estadoFinal)
-print("As transçoes: ",sequenciaAutomato.lerAfd() +"\t|"+" O Estado Final e:",estadoFinal)
+arquivo = lerArquivo()
+pegarEstadoInicial = pegarEstadoInicial(arquivo)
+pegarEstadoFinal = pegarEstadoFinal(arquivo)
+separarAlfabeto = separarAlfabeto(arquivo)
+separarTransicoes = separarTransicoes(arquivo)
+
+sequenciaAutomato = Afd(automato, estadoInicial,alfabeto, transicao, estadoFinal)
+sequenciaAutomatoArquivo = Afd(automato, pegarEstadoInicial, separarAlfabeto, separarTransicoes, pegarEstadoFinal)
+
+print("Setando todas as Informaçoes \n")
+print("As transçoes: ", sequenciaAutomato.lerAfd() + "\t|"+" O Estado Final e:", estadoFinal)
+
 sequencialAutomato = sequenciaAutomato.lerAfd()
 print("Esse Automato e: ", sequenciaAutomato.validarAfd(sequencialAutomato))
+
+
+print("\n\nUtilizando o Arquivo\n")
+print("As transçoes: ", sequenciaAutomatoArquivo.lerAfd() + "\t|"+" O Estado Final e:", pegarEstadoFinal)
+
+sequencialAutomatoArquivo = sequenciaAutomatoArquivo.lerAfd()
+print("Esse Automato e: ", sequenciaAutomatoArquivo.validarAfd(sequencialAutomatoArquivo))
